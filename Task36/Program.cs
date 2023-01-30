141f99fdba943c2f1f71886c24cebdf24adffdb7
Slava <mistik99@mail.ru>
2023-01-30 21:23:46 +0200

    Задача 36

---
+++ Task36/Program.cs
@@ -0,0 +1,33 @@
+﻿int [] InitArray(int dimension)
+{
+    int[] arr = new int[dimension];
+    Random rnd = new Random();
+
+    for (int i = 0; i < dimension; i++)
+    {
+        arr[i] = rnd.Next(0, 9);
+    }
+    return arr;
+}
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+int GetNumber(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(i % 2 != 0)
+            count += array[i];
+    }
+    return count;
+}
+int[] arr = InitArray(6);
+PrintArray(arr);
+int a =GetNumber(arr);
+Console.WriteLine(a);
